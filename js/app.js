831f480ce011e7cc169405c95109b200ecac11a1
Nathanael Fletcher <nathanaelfletcher19@gmail.com>
2020-02-08 21:46:35 -0500

    created working carousel

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@
 
 let accessToken = '';
 let currentGame = [];
+let questionNumber = 0;
+let $nextButton;
+let $prevButton;
 
 //Global Functions
 
@@ -11,6 +14,29 @@ const shuffleArray = (array) => {
         [array[i], array[j]] = [array[j], array[i]];
     }
 };
+
+const initButtons = () => {
+    $prevButton = $('<button>').text("<").appendTo('.questions');
+    $nextButton = $('<button>').text(">").appendTo('.questions');
+    $nextButton.on('click', nextWindow);
+    $prevButton.on('click', prevWindow);
+}
+
+const nextWindow = () => {
+    $(`.question-${questionNumber}`).hide();
+    if (questionNumber < currentGame.length - 1) {
+        questionNumber++;
+    };
+    $(`.question-${questionNumber}`).show();
+}
+
+const prevWindow = () => {
+    $(`.question-${questionNumber}`).hide();
+    if (questionNumber > 0) {
+        questionNumber--
+    };
+    $(`.question-${questionNumber}`).show();
+}
 
 // Acquire access token and store it under accessToken. Will not work immediately on page load.
 $.ajax({
@@ -24,7 +50,8 @@ $.ajax({
 // Creates a new quiz
 
 const newGame = () => {
-    $('.questions').empty();
+    $('.questions').empty(); //Resets div
+    initButtons();
     $.ajax({
         url: 'https://opentdb.com/api.php?amount=10&token=' + accessToken
     }).then((data) => {
@@ -32,24 +59,18 @@ const newGame = () => {
         for (let i = 0; i < 10; i++) {
             currentGame.push(data.results[i]);
         }
-        console.log(currentGame);
-        currentGame.forEach((item)=>{
-            console.log("is running");
-            let $newForm = $('<form>');
+        for (i=0; i < currentGame.length; i++){
+            let $newForm = $('<form>').addClass(`question-${i}`);
             $('.questions').append($newForm);
-            let $newQuestion = $('<h2>').html(item.question).appendTo($newForm);
-            console.log($newQuestion);
-            let $correctAnswer = $(`<input type="radio" name="question" value="correct" class="correct">${item.correct_answer}</input>`)
-            console.log($correctAnswer);
-            // $newForm.append($correctAnswer);
+            let $newQuestion = $('<h2>').html(currentGame[i].question).appendTo($newForm);
+            let $correctAnswer = $(`<input type="radio" name="question" value="correct" class="correct">${currentGame[i].correct_answer}</input>`)
             let currentQuestion = [];
             currentQuestion.push($correctAnswer);
-            for (x of item.incorrect_answers) {
+            for (x of currentGame[i].incorrect_answers) {
                 let $currentAnswer = $(`<input type="radio" name="question" value="incorrect">${x}</input>`)
                 currentQuestion.push($currentAnswer);
             }
             shuffleArray(currentQuestion);
-            console.log(currentQuestion);
             for (x of currentQuestion) {
                 $newForm.append(x);
             }
@@ -60,17 +81,16 @@ const newGame = () => {
             $newForm.on('submit', (event) => {
                 event.preventDefault();
                 $submit.attr('disabled', true);
-                // console.log($(event.target));
-                // $(event.target).addClass('clicked');
                 $newForm.find(':radio:not(:checked)').attr('disabled', true);
-                console.log($newForm.find(':radio:checked').val());
                 if ($newForm.find(':radio:checked').val() === 'correct') {
                     $(event.currentTarget).addClass('answered-correct');
                 } else {
                     $(event.currentTarget).addClass('answered-incorrect');
                 }
             })
-        })
+            $newForm.hide();
+        }
+        $(`.question-${questionNumber}`).show();
     }, ()=> {
         console.log("F");
     })
@@ -82,3 +102,92 @@ $('#new-game').on('click', newGame);
 
 
 })
+
+// old Code
+// const newGame = () => {
+//     $('.questions').empty(); //Resets div
+//     $.ajax({
+//         url: 'https://opentdb.com/api.php?amount=10&token=' + accessToken
+//     }).then((data) => {
+//         currentGame = [];
+//         for (let i = 0; i < 10; i++) {
+//             currentGame.push(data.results[i]);
+//         }
+//         currentGame.forEach((item)=>{
+//             let $newForm = $('<form>');
+//             $('.questions').append($newForm);
+//             let $newQuestion = $('<h2>').html(item.question).appendTo($newForm);
+//             let $correctAnswer = $(`<input type="radio" name="question" value="correct" class="correct">${item.correct_answer}</input>`)
+//             let currentQuestion = [];
+//             currentQuestion.push($correctAnswer);
+//             for (x of item.incorrect_answers) {
+//                 let $currentAnswer = $(`<input type="radio" name="question" value="incorrect">${x}</input>`)
+//                 currentQuestion.push($currentAnswer);
+//             }
+//             shuffleArray(currentQuestion);
+//             for (x of currentQuestion) {
+//                 $newForm.append(x);
+//             }
+//             let $submit = $('<input type="submit" value="submit" disabled=true>').appendTo($newForm);
+//             $newForm.on('input', (event)=> {
+//                 $submit.attr('disabled', false);
+//             })
+//             $newForm.on('submit', (event) => {
+//                 event.preventDefault();
+//                 $submit.attr('disabled', true);
+//                 $newForm.find(':radio:not(:checked)').attr('disabled', true);
+//                 if ($newForm.find(':radio:checked').val() === 'correct') {
+//                     $(event.currentTarget).addClass('answered-correct');
+//                 } else {
+//                     $(event.currentTarget).addClass('answered-incorrect');
+//                 }
+//             })
+//         })
+//     }, ()=> {
+//         console.log("F");
+//     })
+// }
+
+
+// attempt 2 - delete and recreate divs
+// const newGame = () => {
+//     $('.questions').empty();
+//     $.ajax({
+//         url: 'https://opentdb.com/api.php?amount=10&token=' + accessToken
+//     }).then((data) => {
+//         currentGame = [];
+//         for (let i = 0; i < 10; i++) {
+//             currentGame.push(data.results[i]);
+//         }
+//         let $newForm = $('<form>');
+//         $('.questions').append($newForm);
+//         let $newQuestion = $('<h2>').html(currentGame[questionNumber].question).appendTo($newForm);
+//         let $correctAnswer = $(`<input type="radio" name="question" value="correct" class="correct">${currentGame[questionNumber].correct_answer}</input>`)
+//         let currentQuestion = [];
+//         currentQuestion.push($correctAnswer);
+//         for (x of currentGame[questionNumber].incorrect_answers) {
+//             let $currentAnswer = $(`<input type="radio" name="question" value="incorrect">${x}</input>`)
+//             currentQuestion.push($currentAnswer);
+//             }
+//         shuffleArray(currentQuestion);
+//         for (x of currentQuestion) {
+//             $newForm.append(x);
+//         }
+//         let $submit = $('<input type="submit" value="submit" disabled=true>').appendTo($newForm);
+//         $newForm.on('input', (event)=> {
+//             $submit.attr('disabled', false);
+//         })
+//         $newForm.on('submit', (event) => {
+//             event.preventDefault();
+//             $submit.attr('disabled', true);
+//             $newForm.find(':radio:not(:checked)').attr('disabled', true);
+//             if ($newForm.find(':radio:checked').val() === 'correct') {
+//                 $(event.currentTarget).addClass('answered-correct');
+//             } else {
+//                 $(event.currentTarget).addClass('answered-incorrect');
+//             }
+//         })
+//     }, ()=> {
+//         console.log("F");
+//     })
+// }
