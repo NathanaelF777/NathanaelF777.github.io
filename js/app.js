7cf0c2c6060cf1d1e3eaa02d0f140d3caddf9f20
Nathanael Fletcher <nathanaelfletcher19@gmail.com>
2020-02-12 14:28:16 -0500

    changed CSS due to weird submit button on mobile.

--- js/app.js
+++ js/app.js
@@ -128,7 +128,7 @@ const newGame = () => {
             for (x of currentQuestion) {
                 $newForm.append(x);
             }
-            let $submit = $('<span><input type="submit" value="submit" disabled=true  class="resetScoreBtn"></span>').appendTo($newForm);
+            let $submit = $('<input type="submit" value="submit" disabled=true  class="resetScoreBtn">').appendTo($newForm);
             $newForm.on('input', (event)=> {
                 $submit.attr('disabled', false);
             })
