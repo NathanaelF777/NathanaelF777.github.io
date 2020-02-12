c1c714d91678b4ab335e399176e418ff2e385351
Nathanael Fletcher <nathanaelfletcher19@gmail.com>
2020-02-12 17:21:42 -0500

    trying again

--- js/app.js
+++ js/app.js
@@ -128,7 +128,7 @@ const newGame = () => {
             for (x of currentQuestion) {
                 $newForm.append(x);
             }
-            let $submit = $('<input type="submit" value="submit" disabled=true>').appendTo($newDiv);
+            let $submit = $('<input type="submit" value="submit" disabled=true class="question-submit">').appendTo($newForm);
             $newForm.on('input', (event)=> {
                 $submit.attr('disabled', false);
             })
